a7ebf3ba94df5976bf0c894bced3b9039706280f
Dayo Akinbami <dayoakinbami@Dayos-MacBook-Pro.local>
2024-11-17 14:57:09 +0100

    remove serverless files

--- .serverless_nextjs/assets/public/js/datatable.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#example').DataTable();
-
-    $('#example tbody').on( 'click', 'tr', function () {
-        $(this).toggleClass('selected');
-    } );
- 
-} );
